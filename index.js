2a596e3134834e566139f7743e61158ebb4141e9
bundi <robermagara66@gmail.com>
2025-02-21 10:05:49 +0000

    add latest changes

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from 'express';
-
 const app = express();
+
+import { genkit } from 'genkit';
+import { gemini15Flash, vertexAI } from '@genkit-ai/vertexai';
+
+const ai = genkit({
+    plugins: [
+        vertexAI({ location: 'us-central1' }),
+    ],
+});
+
 app.get('/', async (req, res) => {
-    res.send('Hello world!');
+    const animal = req.query.animal || 'dog';
+    const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
+    const llmResponse = await ai.generate({
+        model: gemini15Flash,
+        prompt: prompt,
+    });
+    const html = llmResponse.text;
+    res.send(html);
 });
 
 const port = process.env.PORT || 8080;
